c52de267f1d0c2fccc821038dbb6d44603c1731f
unknown <bsd1421@gmail.com>
2024-02-10 21:50:58 +0300

    Add backspace

--- calculate.js
+++ calculate.js
@@ -8,6 +8,23 @@ function clearDisplay() {
 
 function calculate() {
     const expression = document.getElementById('display').value;
-    const result = eval(expression);
+    let result;
+    if (expression.includes('%')) {
+        // Разделяем выражение на число и процентное значение
+        const [numStr, percentStr] = expression.split('%');
+        const num = parseFloat(numStr);
+        const percent = parseFloat(percentStr) / 100;
+        // Вычисляем процент от числа
+        result = num * percent;
+    } else {
+        // Используем eval для вычисления обычного выражения
+        result = eval(expression);
+    }
     document.getElementById("display").value = result;
+}
+
+function removeLastCharacter() {
+    const display = document.getElementById("display"); // Получаем доступ к полю отображения
+    const currentValue = display.value; // Получаем текущее значение из поля отображения
+    display.value = currentValue.slice(0, -1); // Удаляем последний символ и устанавливаем обновленное значение
 }
